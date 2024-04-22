34eecf65bebbfff4c9c405615130944125e18335
sapphinebz <sapphinebz@gmail.com>
2024-04-22 21:17:43 +0700

    schedule timer to keep alive on trading-view to service worker

--- content-scripts/huasengheng/receive-transactions-from-sw.ts
+++ content-scripts/huasengheng/receive-transactions-from-sw.ts
@@ -1,16 +1,32 @@
-import { Observable } from "rxjs";
+import { Observable, Subject, timer } from "rxjs";
+import { switchMap } from "rxjs/operators";
 import { TransactionChange } from "./models/transaction-change.model";
 import { currentThaiTime } from "../utils/current-thai-time";
 export const TRANSACTION_CHANGES_RECEVING = "transactionChangesReceiving";
 
-export function receiveTransactionsFromSW() {
+export function transactionsChangesFromSW() {
   return new Observable<TransactionChange>((subscriber) => {
     // say hi
     chrome.runtime.sendMessage({ name: "tradingView" });
-    chrome.runtime.onMessage.addListener((records: TransactionChange) => {
+
+    // timer to keep connections
+    const onUpdated = new Subject<void>();
+    const suptime = onUpdated
+      .pipe(switchMap(() => timer(10000)))
+      .subscribe(() => {
+        chrome.runtime.sendMessage({ name: "tradingView" });
+      });
+
+    subscriber.add(suptime);
+    const callback = (records: TransactionChange) => {
       console.log(`sw`, records, `${currentThaiTime()}`);
       subscriber.next(records);
+      onUpdated.next();
       return undefined;
+    };
+    chrome.runtime.onMessage.addListener(callback);
+    subscriber.add(() => {
+      chrome.runtime.onMessage.removeListener(callback);
     });
   });
 }
