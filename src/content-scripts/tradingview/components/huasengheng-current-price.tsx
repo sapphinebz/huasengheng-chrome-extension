ba7c76c86ca6e2b4beb9e90044edf344aa6c4e79
sapphinebz <sapphinebz@gmail.com>
2024-05-18 00:08:49 +0700

    migrate to React

--- src/content-scripts/tradingview/components/huasengheng-current-price.tsx
+++ src/content-scripts/tradingview/components/huasengheng-current-price.tsx
@@ -8,66 +8,68 @@ interface ComponentProps {
   transactionChange$: Observable<TransactionChange>;
   visible: boolean;
 }
-const HuasenghengCurrentPrice = React.memo((props: ComponentProps) => {
-  const { transactionChange$ } = props;
+const HuasenghengCurrentPrice: React.FC<ComponentProps> = React.memo(
+  (props) => {
+    const { transactionChange$ } = props;
 
-  const [transactionChanges, setTransactionChanges] =
-    useState<TransactionChange>();
+    const [transactionChanges, setTransactionChanges] =
+      useState<TransactionChange>();
 
-  const buyPrice = useMemo(() => {
-    if (transactionChanges) {
-      const { huasenghengBuy } = transactionChanges;
-      return formatCurrencyWithoutSymbol(huasenghengBuy);
-    }
-    return "";
-  }, [transactionChanges?.huasenghengBuy]);
+    const buyPrice = useMemo(() => {
+      if (transactionChanges) {
+        const { huasenghengBuy } = transactionChanges;
+        return formatCurrencyWithoutSymbol(huasenghengBuy);
+      }
+      return "";
+    }, [transactionChanges?.huasenghengBuy]);
 
-  const sellPrice = useMemo(() => {
-    if (transactionChanges) {
-      const { huasenghengSell } = transactionChanges;
-      return formatCurrencyWithoutSymbol(huasenghengSell);
-    }
-    return "";
-  }, [transactionChanges?.huasenghengSell]);
+    const sellPrice = useMemo(() => {
+      if (transactionChanges) {
+        const { huasenghengSell } = transactionChanges;
+        return formatCurrencyWithoutSymbol(huasenghengSell);
+      }
+      return "";
+    }, [transactionChanges?.huasenghengSell]);
 
-  useEffect(() => {
-    const subscription = transactionChange$.subscribe((changes) =>
-      setTransactionChanges(changes)
+    useEffect(() => {
+      const subscription = transactionChange$.subscribe((changes) =>
+        setTransactionChanges(changes)
+      );
+      return () => {
+        subscription.unsubscribe();
+      };
+    }, [transactionChange$]);
+
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      const el = containerRef.current;
+      if (el) {
+        const subscription = makeItMovable(el).subscribe();
+        return () => subscription.unsubscribe();
+      }
+    }, [containerRef]);
+
+    const nodeClassName = useMemo(
+      () => (props.visible ? "" : "chrome-hide"),
+      [props.visible]
     );
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, [transactionChange$]);
 
-  const containerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const el = containerRef.current;
-    if (el) {
-      const subscription = makeItMovable(el).subscribe();
-      return () => subscription.unsubscribe();
-    }
-  }, [containerRef]);
-
-  const nodeClassName = useMemo(
-    () => (props.visible ? "" : "chrome-hide"),
-    [props.visible]
-  );
-
-  return (
-    <div
-      ref={containerRef}
-      className={`chrome-hua-fixed chrome-hua-font ${nodeClassName}`}
-    >
-      <div>
-        <div className="chrome-hua-header-buy">รับซื้อ</div>
-        <div className="chrome-hua-value-buy">{buyPrice}</div>
+    return (
+      <div
+        ref={containerRef}
+        className={`chrome-hua-fixed chrome-hua-font ${nodeClassName}`}
+      >
+        <div>
+          <div className="chrome-hua-header-buy">รับซื้อ</div>
+          <div className="chrome-hua-value-buy">{buyPrice}</div>
+        </div>
+        <div>
+          <div className="chrome-hua-header-sell">ขายออก</div>
+          <div className="chrome-hua-value-sell">{sellPrice}</div>
+        </div>
       </div>
-      <div>
-        <div className="chrome-hua-header-sell">ขายออก</div>
-        <div className="chrome-hua-value-sell">{sellPrice}</div>
-      </div>
-    </div>
-  );
-});
+    );
+  }
+);
 export default HuasenghengCurrentPrice;
